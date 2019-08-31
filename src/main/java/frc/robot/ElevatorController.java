256f7ab346bc75d48ffd6ff67c7168b6c2f57718
Ishan Madan <ishan@mistlogic.com>
2019-08-30 23:08:29 -0700

    Made Intake and Elevator Controllers into acceptable classes

---
+++ src/main/java/frc/robot/ElevatorController.java
@@ -0,0 +1,21 @@
+package frc.robot;
+
+/**
+ * @author ishanmadan
+ */
+public class ElevatorController implements RobotController {
+    public ElevatorController() {
+
+    }
+
+    @Override
+    public String getName() {
+        return "ElevatorController";
+    }
+
+    @Override
+    public boolean performAction(RobotProperties properties) {
+        
+        return true;
+    }
+}
--- src/main/java/frc/robot/IntakeController.java
+++ src/main/java/frc/robot/IntakeController.java
@@ -16,5 +16,6 @@ public class IntakeController implements RobotController {
     @Override
     public boolean performAction(RobotProperties properties) {
         
+        return true;
     }
 }
