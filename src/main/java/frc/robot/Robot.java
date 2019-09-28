d08840fba146ea0820ba8eeacc2e2618783f9449
Popcorn Penguins <popcornpenguins6238@gmail.com>
2019-09-28 16:34:52 -0700

    fixed an improper comment

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -37,8 +37,7 @@ public class Robot extends TimedRobot {
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
-   */
-  @Override
+  */
   public void robotInit() {
     properties = new RobotProperties();
     properties.gyro.calibrate();
